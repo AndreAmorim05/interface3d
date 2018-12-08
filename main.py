8e839590c5deaf9031f5d46632c9cd3a0a8e6509
André Amorim <js.andreamorim@gmail.com>
2018-12-07 22:41:26 -0300

    Iníciando o projeto

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App 
+from kivy.animation import Animation
+from kivy.graphics.transformation import Matrix
+
+class Ambiente3D(App): 
+    pass
+
+if __name__ == '__main__': 
+    Ambiente3D().run()
